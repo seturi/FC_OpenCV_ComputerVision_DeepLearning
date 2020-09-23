8799b3ed1c68c87de2154b1782d312b39bd8a074
seturi <seturi06@gmail.com>
2020-09-23 12:47:01 +0900

    11th Commit

---
+++ ch06/canny.py
@@ -0,0 +1,17 @@
+import sys
+import cv2
+
+
+src = cv2.imread('.\\ch06\\building.jpg', cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    print('Image load failed!')
+    sys.exit()
+
+dst = cv2.Canny(src, 50, 150)
+
+cv2.imshow('src', src)
+cv2.imshow('dst', dst)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
