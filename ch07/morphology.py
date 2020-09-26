98f46ffc4e7f80088fa9bb7d6f648e24a8675a44
seturi <seturi06@gmail.com>
2020-09-26 15:21:29 +0900

    14th Commit

---
+++ ch07/morphology.py
@@ -0,0 +1,20 @@
+import sys
+import cv2
+
+
+src = cv2.imread('.\\ch07\\circuit.bmp', cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    print('Image load failed!')
+    sys.exit()
+
+se = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 3))
+dst1 = cv2.erode(src, se)
+
+dst2 = cv2.dilate(src, None)
+
+cv2.imshow('src', src)
+cv2.imshow('dst1', dst1)
+cv2.imshow('dst2', dst2)
+cv2.waitKey()
+cv2.destroyAllWindows()
